5232a056ba75a9635c6b649fbfafd0a22a431620
zetlen <jzetlen@adobe.com>
2019-04-29 12:23:17 -0500

    chore: remove shrink-ray fork now that shrink-ray-current merged my PR

--- packages/upward-js/lib/bestPractices.js
+++ packages/upward-js/lib/bestPractices.js
@@ -2,7 +2,7 @@ const connect = require('connect');
 let shrinkRay;
 
 try {
-    shrinkRay = require('@magento/shrink-ray');
+    shrinkRay = require('shrink-ray-current');
 } catch (error) {
     shrinkRay = false;
 }
@@ -15,7 +15,7 @@ function bestPractices() {
         bestPracticeMiddlewares.use(shrinkRay());
     } catch (e) {
         console.warn(
-            `Cannot add compression middleware: dependency \`@magento/shrink-ray\` is not
+            `Cannot add compression middleware: dependency \`shrink-ray-current\` is not
 installed or not compatible with this environment. Assets will be served uncompressed.
 
 If possible, install additional tools to build NodeJS native dependencies:
